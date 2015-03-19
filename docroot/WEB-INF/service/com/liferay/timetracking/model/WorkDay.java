fd42aafee702e18c1c1d8064cf88c1ffd74ae8a7
istvansajtos <istvan.sajtos@liferay.com>
2015-03-19 21:40:41 +0100

    issue#2 Building service layer to WorkDay entity

---
+++ docroot/WEB-INF/service/com/liferay/timetracking/model/WorkDay.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.timetracking.model;
+
+import com.liferay.portal.model.PersistedModel;
+
+/**
+ * The extended model interface for the WorkDay service. Represents a row in the &quot;TimeTracking_WorkDay&quot; database table, with each column mapped to a property of this class.
+ *
+ * @author Istvan Sajtos
+ * @see WorkDayModel
+ * @see com.liferay.timetracking.model.impl.WorkDayImpl
+ * @see com.liferay.timetracking.model.impl.WorkDayModelImpl
+ * @generated
+ */
+public interface WorkDay extends WorkDayModel, PersistedModel {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add methods to {@link com.liferay.timetracking.model.impl.WorkDayImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+}
